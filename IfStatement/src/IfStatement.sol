5f44833ccb52411b2d59d5960fd1a450de04b18a
vishnu <vspn001@gmail.com>
2024-12-31 16:02:10 +0530

    Solidity projects

---
+++ IfStatement/src/IfStatement.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract IfStatement {
+    function max(uint256 a, uint256 b) public pure returns (uint256) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    function min(uint256 a, uint256 b) public pure returns (uint256) {
+        if (a < b) {
+        return a;
+       } else {
+        return b;
+       }
+    
+    }
+}
