70b75389f1800d95aa0eeaba4f2f79978f45d02b
vishnu <vspn001@gmail.com>
2025-01-01 12:31:59 +0530

    array

--- SumArray/src/SumArray.sol
+++ SumArray/src/SumArray.sol
@@ -2,10 +2,17 @@
 pragma solidity ^0.8.13;
 
 contract SumArray {
-    function sumArray(uint256[] calldata arr) public pure returns (uint256) {
-        // your code here
-        // arr is a list of unsigned integers
-        // return the sum of them. If the array
-        // is empty, return 0
+
+     function returnFirstElement(uint256[] calldata arr) public pure returns (uint256) {
+
+        require(arr.length > 0, "Length of array should be greater than 0");
+        uint256 product = 1;
+
+        for(uint256 i = 1; i <= arr.length; i++){
+            product *= arr[i];
+        }
+
+        return product;
+       
     }
 }
